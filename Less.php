24ebef75e986893cf52147910b503d121e792e96
Ra2ven27 <raven210__@hotmail.com>
2024-10-09 14:41:54 +0200

    Create Less.php

---
+++ Less.php
@@ -0,0 +1,18 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+</head>
+
+<body style="background-color:black;">
+  <div style="width:1000px; background-color:white;">
+    <div>
+      <div style="width:1000px; float:left; font-size:50px; color:white;">
+        <p id="" style="float:left; margin-top:30px; text-align:center; width:1000px;font-size:50px;">
+        Selected text size: 50px. Less than 3 rows. Proof. Less than 3 rows.Less than 3 rows.
+        </p>
+      </div>      
+    </div>
+  </div>
+</body>
+</html>
